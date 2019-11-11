ed4c2e11f394656f00aeb84c5f0dcbca220c2254
JusebacruBC <51666668+JusebacruBC@users.noreply.github.com>
2019-11-11 16:22:41 -0500

    Update WebServicesRestLogin.java

--- src/main/java/com/Finden/findenBackEnd/WS/WebServicesRestLogin.java
+++ src/main/java/com/Finden/findenBackEnd/WS/WebServicesRestLogin.java
@@ -1,16 +1,22 @@
 package com.Finden.findenBackEnd.WS;
-
+/*
+*Librerías encargadas de la creación del controlador REST de la aplicación FINDEN
+*/
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/*
+*Librerías encargados del consumo de servicios deñ controlador
+*/
 import com.Finden.findenBackEnd.models.entity.Request;
 import com.Finden.findenBackEnd.models.entity.User;
 import com.Finden.findenBackEnd.models.service.FacadeLogin;
-
+/*
+*Sentencia utilizada para la conexión del Facelet de LogIn
+*/
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/finden")
